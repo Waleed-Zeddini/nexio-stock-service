fb6c47a15c8ad3899f451c7827895168525c4010
Waleed-Zeddini <waleed.zeddini@gmail.com>
2021-11-19 23:32:31 +0100

    Feign To Order Serive ( Giving Client)

--- src/main/java/com/nexio/api/ms/StockService.java
+++ src/main/java/com/nexio/api/ms/StockService.java
@@ -3,9 +3,11 @@ package com.nexio.api.ms;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 //import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 //@EnableEurekaClient         // To enable eureka client
+@EnableFeignClients // To enable Open Feign client
 public class StockService {
 
 	public static void main(String[] args) {
